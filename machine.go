647e1331c3fdf85b4f77a5e7aa53129d0a267bcd
Scott Ferguson <scottwferg@gmail.com>
2012-09-27 17:49:02 -0500

    Use game name in window title

--- machine.go
+++ machine.go
@@ -21,7 +21,8 @@ var (
 	video      Video
 	controller Controller
 
-    game string
+	gamename string
+	game     string
 )
 
 func setResetVector() {
@@ -34,110 +35,110 @@ func setResetVector() {
 func LoadState() {
 	fmt.Println("Loading state")
 
-    state, err := ioutil.ReadFile(game)
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
+	state, err := ioutil.ReadFile(game)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-    for i, v := range state[:0x2000] {
-        Ram[i] = Word(v)
-    }
+	for i, v := range state[:0x2000] {
+		Ram[i] = Word(v)
+	}
 
-    pchigh := int(state[0x2000])
-    pclow := int(state[0x2001])
+	pchigh := int(state[0x2000])
+	pclow := int(state[0x2001])
 
-    ProgramCounter = (pchigh << 8) | pclow
+	ProgramCounter = (pchigh << 8) | pclow
 
-    cpu.A = Word(state[0x2002])
-    cpu.X = Word(state[0x2003])
-    cpu.Y = Word(state[0x2004])
-    cpu.P = Word(state[0x2005])
-    cpu.StackPointer = Word(state[0x2006])
+	cpu.A = Word(state[0x2002])
+	cpu.X = Word(state[0x2003])
+	cpu.Y = Word(state[0x2004])
+	cpu.P = Word(state[0x2005])
+	cpu.StackPointer = Word(state[0x2006])
 
-    // Sprite RAM
-    for i, v := range state[0x2007:0x2107] {
-        ppu.SpriteRam[i] = Word(v)
-    }
+	// Sprite RAM
+	for i, v := range state[0x2007:0x2107] {
+		ppu.SpriteRam[i] = Word(v)
+	}
 
-    // Pattern VRAM
-    for i, v := range state[0x2107:0x4107] {
-        ppu.Vram[i] = Word(v)
-    }
+	// Pattern VRAM
+	for i, v := range state[0x2107:0x4107] {
+		ppu.Vram[i] = Word(v)
+	}
 
-    // Nametable VRAM
-    for i, v := range state[0x4107:0x4507] {
-        ppu.Nametables.LogicalTables[0][i] = Word(v)
-    }
-    for i, v := range state[0x4507:0x4907] {
-        ppu.Nametables.LogicalTables[1][i] = Word(v)
-    }
-    for i, v := range state[0x4907:0x4D07] {
-        ppu.Nametables.LogicalTables[2][i] = Word(v)
-    }
-    for i, v := range state[0x4D07:0x5107] {
-        ppu.Nametables.LogicalTables[3][i] = Word(v)
-    }
+	// Nametable VRAM
+	for i, v := range state[0x4107:0x4507] {
+		ppu.Nametables.LogicalTables[0][i] = Word(v)
+	}
+	for i, v := range state[0x4507:0x4907] {
+		ppu.Nametables.LogicalTables[1][i] = Word(v)
+	}
+	for i, v := range state[0x4907:0x4D07] {
+		ppu.Nametables.LogicalTables[2][i] = Word(v)
+	}
+	for i, v := range state[0x4D07:0x5107] {
+		ppu.Nametables.LogicalTables[3][i] = Word(v)
+	}
 
-    // Palette RAM
-    for i, v := range state[0x5107:0x5126] {
-        ppu.PaletteRam[i] = Word(v)
-    }
+	// Palette RAM
+	for i, v := range state[0x5107:0x5126] {
+		ppu.PaletteRam[i] = Word(v)
+	}
 }
 
 func SaveState() {
 	fmt.Println("Saving state")
-    buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)
 
-    // RAM
-    for _, v := range Ram[:0x2000] {
-        buf.WriteByte(byte(v))
-    }
+	// RAM
+	for _, v := range Ram[:0x2000] {
+		buf.WriteByte(byte(v))
+	}
 
-    // ProgramCounter
-    // High then low
-    buf.WriteByte(byte(ProgramCounter >> 8) & 0xFF)
-    buf.WriteByte(byte(ProgramCounter & 0xFF))
+	// ProgramCounter
+	// High then low
+	buf.WriteByte(byte(ProgramCounter>>8) & 0xFF)
+	buf.WriteByte(byte(ProgramCounter & 0xFF))
 
-    // CPU Registers
-    buf.WriteByte(byte(cpu.A))
-    buf.WriteByte(byte(cpu.X))
-    buf.WriteByte(byte(cpu.Y))
-    buf.WriteByte(byte(cpu.P))
-    buf.WriteByte(byte(cpu.StackPointer))
+	// CPU Registers
+	buf.WriteByte(byte(cpu.A))
+	buf.WriteByte(byte(cpu.X))
+	buf.WriteByte(byte(cpu.Y))
+	buf.WriteByte(byte(cpu.P))
+	buf.WriteByte(byte(cpu.StackPointer))
 
-    // Sprite RAM
-    for _, v := range ppu.SpriteRam {
-        buf.WriteByte(byte(v))
-    }
+	// Sprite RAM
+	for _, v := range ppu.SpriteRam {
+		buf.WriteByte(byte(v))
+	}
 
-    // Pattern VRAM
-    for _, v := range ppu.Vram[:0x2000] {
-        buf.WriteByte(byte(v))
-    }
+	// Pattern VRAM
+	for _, v := range ppu.Vram[:0x2000] {
+		buf.WriteByte(byte(v))
+	}
 
-    // Nametable VRAM
-    for _, v := range ppu.Nametables.LogicalTables[0] {
-        buf.WriteByte(byte(v))
-    }
-    for _, v := range ppu.Nametables.LogicalTables[1] {
-        buf.WriteByte(byte(v))
-    }
-    for _, v := range ppu.Nametables.LogicalTables[2] {
-        buf.WriteByte(byte(v))
-    }
-    for _, v := range ppu.Nametables.LogicalTables[3] {
-        buf.WriteByte(byte(v))
-    }
+	// Nametable VRAM
+	for _, v := range ppu.Nametables.LogicalTables[0] {
+		buf.WriteByte(byte(v))
+	}
+	for _, v := range ppu.Nametables.LogicalTables[1] {
+		buf.WriteByte(byte(v))
+	}
+	for _, v := range ppu.Nametables.LogicalTables[2] {
+		buf.WriteByte(byte(v))
+	}
+	for _, v := range ppu.Nametables.LogicalTables[3] {
+		buf.WriteByte(byte(v))
+	}
 
-    // Palette RAM
-    for _, v := range ppu.PaletteRam {
-        buf.WriteByte(byte(v))
-    }
+	// Palette RAM
+	for _, v := range ppu.PaletteRam {
+		buf.WriteByte(byte(v))
+	}
 
-    if err := ioutil.WriteFile(game, buf.Bytes(), 0644); err != nil {
-        panic(err.Error())
-    }
+	if err := ioutil.WriteFile(game, buf.Bytes(), 0644); err != nil {
+		panic(err.Error())
+	}
 }
 
 func main() {
@@ -157,10 +158,10 @@ func main() {
 			return
 		}
 
-        // Set the game name for save states
-        path := strings.Split(os.Args[1], "/")
-        gamename := strings.Split(path[len(path) - 1], ".")[0]
-        game = fmt.Sprintf(".%s.state", gamename)
+		// Set the game name for save states
+		path := strings.Split(os.Args[1], "/")
+		gamename = strings.Split(path[len(path)-1], ".")[0]
+		game = fmt.Sprintf(".%s.state", gamename)
 
 		setResetVector()
 	} else {
@@ -168,7 +169,7 @@ func main() {
 		return
 	}
 
-	video.Init(v, d, os.Args[1])
+	video.Init(v, d, gamename)
 	defer video.Close()
 
 	go JoypadListen()
