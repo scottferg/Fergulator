01d83c42b7f23545747b2aaaf74744838cb9f504
Scott Ferguson <scottwferg@gmail.com>
2012-10-07 18:02:12 -0500

    Initial work on MMC3, still very broken

--- rom.go
+++ rom.go
@@ -6,6 +6,7 @@ import (
 )
 
 const (
+	Size1k  = 0x0400
 	Size4k  = 0x1000
 	Size8k  = 0x2000
 	Size16k = 0x4000
@@ -33,12 +34,31 @@ type Cnrom Rom
 
 func WriteRamBank(rom [][]Word, bank, dest, size int) {
 	for i := 0; i < size; i++ {
+        if i+dest == 0xB59C {
+            fmt.Println("Write to target")
+        }
+		Ram[i+dest] = rom[bank][i]
+	}
+}
+
+// Used by MMC3 for selecting 8kb chunks of a PRG-ROM bank
+func WriteOffsetRamBank(rom [][]Word, bank, dest, size, offset int) {
+	for i := offset; i < size; i++ {
+        if i+dest == 0xB59C {
+            fmt.Println("Write to target")
+        }
 		Ram[i+dest] = rom[bank][i]
 	}
 }
 
 func WriteVramBank(rom [][]Word, bank, dest, size int) {
 	for i := 0; i < size; i++ {
+		ppu.Vram[i+dest] = rom[bank][i]
+	}
+}
+
+func WriteOffsetVramBank(rom [][]Word, bank, dest, size, offset int) {
+	for i := offset; i < size; i++ {
 		ppu.Vram[i+dest] = rom[bank][i]
 	}
 }
@@ -60,7 +80,7 @@ func (m *Unrom) BatteryBacked() bool {
 }
 
 func (m *Cnrom) Write(v Word, a int) {
-	bank := int(v & 0x3) * 2
+	bank := int(v&0x3) * 2
 	WriteVramBank(m.VromBanks, bank, 0x0000, Size4k)
 	WriteVramBank(m.VromBanks, bank+1, 0x1000, Size4k)
 }
@@ -200,6 +220,10 @@ func LoadRom(rom []byte) (m Mapper, e error) {
 			Battery:      r.Battery,
 			Data:         r.Data,
 		}
+	case 0x04:
+		// MMC3
+		fmt.Printf("MMC3\n")
+		m = NewMmc3(r)
 	default:
 		// Unsupported
 		fmt.Printf("Unsupported\n")
