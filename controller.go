33d841b85ba55051afbbaed5b2ccaf330acc356f
Scott Ferguson <scottwferg@gmail.com>
2013-04-23 15:43:41 -0500

    Don't let joypads on shared registers conflict with each others state

--- controller.go
+++ controller.go
@@ -23,52 +23,56 @@ type Controller struct {
 	ButtonState [16]Word
 	StrobeState int
 	LastWrite   Word
-	LastYAxis   int
-	LastXAxis   int
-	BitOffset   int
+	LastYAxis   [2]int
+	LastXAxis   [2]int
 }
 
 func (c *Controller) SetJoypadAxisState(a, d int, v Word, offset int) {
-	resetAxis := func(d int) {
+	resetAxis := func(d, i int) {
 		switch d {
 		case 0:
-			if c.LastYAxis != -1 {
-				c.ButtonState[c.LastYAxis+offset] = 0x40
+			if c.LastYAxis[i] != -1 {
+				c.ButtonState[c.LastYAxis[i]] = 0x40
 			}
 		case 1:
-			if c.LastXAxis != -1 {
-				c.ButtonState[c.LastXAxis+offset] = 0x40
+			if c.LastXAxis[i] != -1 {
+				c.ButtonState[c.LastXAxis[i]] = 0x40
 			}
 		}
+	}
+
+	index := 0
+	if offset > 0 {
+		index = 1
 	}
 
 	if a == 4 || a == 1 {
 		switch d {
 		case JoypadAxisUp: // Up
-			resetAxis(0)
+			resetAxis(0, index)
 			c.ButtonState[4+offset] = v
-			c.LastYAxis = 4
+			c.LastYAxis[index] = 4+offset
 		case JoypadAxisDown: // Down
-			resetAxis(0)
+			resetAxis(0, index)
 			c.ButtonState[5+offset] = v
-			c.LastYAxis = 5
+			c.LastYAxis[index] = 5+offset
 		default:
-			resetAxis(0)
-			c.LastYAxis = -1
+			resetAxis(0, index)
+			c.LastYAxis[index] = -1
 		}
 	} else if a == 3 || a == 0 {
 		switch d {
 		case JoypadAxisLeft: // Left
-			resetAxis(1)
+			resetAxis(1, index)
 			c.ButtonState[6+offset] = v
-			c.LastXAxis = 6
+			c.LastXAxis[index] = 6+offset
 		case JoypadAxisRight: // Right
-			resetAxis(1)
+			resetAxis(1, index)
 			c.ButtonState[7+offset] = v
-			c.LastXAxis = 7
+			c.LastXAxis[index] = 7+offset
 		default:
-			resetAxis(1)
-			c.LastXAxis = -1
+			resetAxis(1, index)
+			c.LastXAxis[index] = -1
 		}
 	}
 }
@@ -96,8 +100,6 @@ func (c *Controller) SetButtonState(k sdl.KeyboardEvent, v Word, offset int) {
 		c.ButtonState[2+offset] = v
 	case sdl.K_RETURN: // Start
 		c.ButtonState[3+offset] = v
-	case sdl.K_m: // Start
-		c.ButtonState[11] = v
 	case sdl.K_UP: // Up
 		c.ButtonState[4+offset] = v
 	case sdl.K_DOWN: // Down
@@ -134,7 +136,7 @@ func (c *Controller) KeyUp(e sdl.KeyboardEvent, offset int) {
 }
 
 func (c *Controller) Write(v Word) {
-	if v == 1 {
+	if v == 0 && c.LastWrite == 1 {
 		// 0x4016 writes manage strobe state for
 		// both controllers. 0x4017 is reserved for
 		// APU
@@ -146,7 +148,7 @@ func (c *Controller) Write(v Word) {
 }
 
 func (c *Controller) Read() (r Word) {
-	if c.StrobeState < 16 {
+	if c.StrobeState < 8 {
 		r = ((c.ButtonState[c.StrobeState+8] & 1) << 1) | c.ButtonState[c.StrobeState]
 	} else if c.StrobeState == 18 {
 		r = 0x0
@@ -188,7 +190,7 @@ func ReadInput(r chan [2]int, i chan int) {
 				var offset int
 				if j > 1 {
 					offset = 8
-					index = j%2
+					index = j % 2
 				}
 
 				switch e.Value {
@@ -207,7 +209,7 @@ func ReadInput(r chan [2]int, i chan int) {
 				var offset int
 				if j > 1 {
 					offset = 8
-					index = j%2
+					index = j % 2
 				}
 
 				switch joy[j].GetButton(int(e.Button)) {
