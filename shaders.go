6f906d33d88668efe360fd6ef49a62a639a92759
Nick Fedesna <nick.fedesna@vokalinteractive.com>
2013-11-23 19:07:33 -0600

    convert palette to vec3 type

--- shaders.go
+++ shaders.go
@@ -14,22 +14,15 @@ const vertShaderSrcDef = `
 const fragShaderSrcDef = `
 	varying vec2 texCoord;
 	uniform sampler2D texture;
-	uniform int palette[64];
+	uniform ivec3 palette[64];
 
 	void main() {
-
 		vec4 t = texture2D(texture, texCoord);
 		int i = int(t.a * 256.0);
 		i = i - ((i / 64) * 64);
-		int p = palette[i];
 
-		ivec3 color;
-		color.r = p / 65536;
-		color.g = (p - color.r * 65536) / 256;
-		color.b = p - color.r * 65536 - color.g * 256;
+		vec3 color = vec3(palette[i]) / 256.0;
 
-		vec3 c;
-		c = vec3(color) / 256.;
-		gl_FragColor = vec4(c.rgb, 1);
+		gl_FragColor = vec4(color, 1);
 	}
 `
